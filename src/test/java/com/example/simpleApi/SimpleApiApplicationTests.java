8fcc26372d5825425f5da3330ab4693f9fff4bc4
vadim.ablancourt <vadimablancourt683@gmail.com>
2025-02-19 22:31:16 +0100

    initial commit

---
+++ src/test/java/com/example/simpleApi/SimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
